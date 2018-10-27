39e1072d4a1fa54e844357d5918e2d4abe176b5a
John Tillman <john.w.tillman@gmail.com>
2018-10-27 12:09:53 -0400

    search

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,5 +4,14 @@ export default {
   auth: (user) => {
     console.log(user)
     return axios.post('/login', user)
+  },
+  stockSearch (ticker) {
+    const API_KEY = 'NSUNV8LPVSSN0247'
+    const url = 'https://www.alphavantage.co/query?' +
+            'function=TIME_SERIES_DAILY' + 
+            '&symbol=' + ticker +
+            '&apikey=' + API_KEY +
+            '&outputsize=full'
+    return axios.get(url)
   }
 };
