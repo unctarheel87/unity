5c38b04ffbd5fe4931c120f60ebe2413b0446d7c
John Tillman <john.w.tillman@gmail.com>
2018-11-05 21:46:56 -0500

    advisor

---
+++ client/src/pages/AdvisorDash.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Profile extends React.Component {
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Advisor Page</h1>
+      </div>
+    )
+  }
+
+};
